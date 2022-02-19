907c34c7c19fa24bc6ce265a8670a9e2187908b1
Pedro Marins <pedro.silva@meupatrocinio.com>
2022-02-19 18:11:55 -0300

    feat: add dummy components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../interfaces/movie";
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenreTitle: string;
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+    <Header selectedGenreTitle={props.selectedGenreTitle} />
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
